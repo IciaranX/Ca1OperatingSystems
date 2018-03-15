c92b373a028a7b82e464b3f7d3db7c7b7f1f2c52
Ciaran Murphy <D00167978@os-lab.dkit.ie>
2018-03-15 09:54:20 +0000

    Adding in code for jobs.cpp

--- jobs.cpp
+++ jobs.cpp
@@ -1,7 +1,39 @@
 //to code
 #incude "Jobs.h"
 
-String Jobs:()
+Jobs::Jobs()
 {
-  
+  String JobName = "";
+  int ArrivalTime = 0;
+  int JobDuration = 0;
+}
+
+void Jobs::setJobName(String A)
+{
+  JobName = a;
+}
+
+void Jobs::setArrivalTime(int b)
+{
+  ArrivalTime = b;
+}
+
+void Jobs::setJobDuration(int c)
+{
+  JobDuration = c;
+}
+
+String Jobs::getJobName()
+{
+  return JobName;
+}
+
+int Jobs::getArrvalTime()
+{
+  return ArrivalTime;
+}
+
+int Jobs::getJobDuration()
+{
+  return JobDuration;
 }
